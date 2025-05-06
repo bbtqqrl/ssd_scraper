ef9bc7c66491f4d661e5e36cf7f42969cb486018
bbtqqrl <makspeys@gmail.com>
2025-05-06 15:05:13 +0200

    Update fetcher.py

--- scraper/comparer.py
+++ scraper/comparer.py
@@ -1,7 +1,20 @@
 import csv
-from dataclasses import asdict
 from typing import List, Dict
+from dataclasses import asdict
 from schemas import Product
+
+def save_to_csv_moyo(products: List[Dict], filename: str = "products_moyo.csv"):
+    if not products:
+        print("[WARNING] No products to save.")
+        return
+
+    keys = products[0].keys()
+    with open(filename, mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(products)
+
+    print(f"[INFO] Saved {len(products)} products to {filename}")
 
 def save_to_csv(products: List[Product], filename: str = "products.csv"):
     if not products:
--- scraper/fetcher.py
+++ scraper/fetcher.py
@@ -1,10 +1,13 @@
 import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 from typing import Optional
 from parser import extract_products_from_html_moyo
-from comparer import save_to_csv
+from comparer import save_to_csv, save_to_csv_moyo
 from dotenv import load_dotenv
+from schemas import Product
+from datetime import datetime
 import os
 import random
 
@@ -15,88 +18,102 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/136.0.0.0 Safari/537.36",
-    "Referer": "https://hard.rozetka.com.ua/ua/ssd/c80109/",
-    "Origin":  "https://hard.rozetka.com.ua",
-    "X-Requested-With": "XMLHttpRequest",
 }
 
 
-def fetch_html_moyo(url: str, count_pages: int,  timeout: int = 10) -> Optional[str]:
-    html_list = []
-    for page_num in range(count_pages):
-        try:
-            response = requests.get(f"{url}{page_num}", headers=HEADERS, timeout=timeout)
-            response.raise_for_status()
-            html_list.append(response.text)
-        except requests.RequestException as e:
-            print(f"[ERROR] Failed to fetch {url}: {e}")
-            return None
-    return html_list
+class RozetkaScraper:
+    def __init__(self, category_id=80109):
+        self.category_id = category_id
+        self.scraper = cloudscraper.create_scraper(
+            browser={'browser': 'chrome', 'platform': 'windows', 'mobile': False}
+        )
+        self.scraper.headers.update({
+            "User-Agent": "Mozilla/5.0",
+            "Accept-Language": "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7"
+        })
 
+        url = f"https://xl-catalog-api.rozetka.com.ua/v4/goods/get?category_id={self.category_id}"
+        resp = self.scraper.get(url)
+        if resp.status_code != 200:
+            print(resp.status_code)
+            raise Exception(f"Bad status code {resp.status_code}: {resp.text[:100]}")
+        data = resp.json()["data"]
 
-def get_total_pages_moyo(url: str) -> int:
-    try:
-        response = requests.get(url, headers=HEADERS)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "html.parser")
+        self.total_pages = data["total_pages"]
+        self.page_limit = data["goods_limit"]
+        print(f"[INIT] total_pages={self.total_pages}, page_limit={self.page_limit}")
 
-        page_elements = soup.select("ul.pagination li[data-page]")
-        page_numbers = [int(li['data-page']) for li in page_elements if li.has_attr("data-page")]
-
-        if page_numbers:
-            return max(page_numbers)
-        else:
-            print("[WARN] No page numbers found")
-            return 1
-
-    except Exception as e: 
-        print(f"[ERROR] Failed to get total pages: {e}")
-        return 1
+    def get_ids(self):
+        all_ids = []
+        for page in range(1, self.total_pages + 1):
+            url = f"https://xl-catalog-api.rozetka.com.ua/v4/goods/get?category_id={self.category_id}&page={page}"
+            resp = self.scraper.get(url)
+            ids = resp.json()["data"]["ids"]
+            all_ids.extend(ids)
+        return all_ids
     
-# url = "https://www.moyo.ua/ua/comp-and-periphery/inform_carrier/ssd/?page="
-# count_pages = get_total_pages_moyo(url)
-# html = fetch_html_moyo(url, count_pages)
-# products = extract_products_from_html_moyo(html_list=html)
-# save_to_csv(products)
-
-def fetch_html_rozetka(url: str, cookies: dict,  timeout: int = 10) -> Optional[str]:
-    html_list = []
-    # for page_num in range(count_pages):
-    try:
-        response = requests.get(url, headers=HEADERS, cookies=cookies, timeout=timeout, proxies={"http": None, "https": None})
-        response.raise_for_status()
-        html_list.append(response.text)
-    except requests.RequestException as e:
-        print(f"[ERROR] Failed to fetch {url}: {e}")
-        return None
-    return html_list
-
-
-
-def get_rozetka_cookies():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(executable_path="c:/Users/maksp/OneDrive/Документи/chrome-win/chrome.exe", headless=False)  # headless=False = бачиш браузер
-        page = browser.new_page()
-
-        # відкриваємо головну сторінку Rozetka
-        page.goto("https://rozetka.com.ua/")
-
-        page.wait_for_timeout(5000)  # чекаємо 5 секунд — сайт виконає JS, отримає cookies
-
-        cookies = page.context.cookies()
-        browser.close()
-    rozetka_cookies = [
-        c for c in cookies if c["domain"].endswith("rozetka.com.ua")
-    ]
-    cookies_dict = { c["name"]: c["value"] for c in rozetka_cookies }
-    return cookies_dict
-
-cookies = get_rozetka_cookies()
-print(cookies)
-url = "https://xl-catalog-api.rozetka.com.ua/v4/goods/getDetails?country=UA&lang=ua&with_groups=1&with_docket=1&with_extra_info=1&goods_group_href=1&product_ids=354111201,477939314,358532337,234935635,12991699,413068041,387068100,505660054,83374176,429989186,135045840,234908281,277806733,316526614,358521642,93909002,323284093,323271967,53682576,393186270,425511654,131126259,399085038,506294609,506294189,506292129,364609773,234596293,180140570,358539690,417576903,506308029,275080888,383996817,266667191,442859777,395441364,453864368,441843560,506291429,396907284,503392249,316527697,131123333,436709108,504658874,364600170,503384109,484458999,504658919,450999611,437833892,408830295,422630754,381999168,422630745,484456084,9971798,390507354,41520760"
-data = fetch_html_rozetka(url=url, cookies=cookies)
-print(data)
+    def get_items(self, item_list):
+        product_list = []
+        while item_list:
+            batch = item_list[:self.page_limit]
+            product_ids = ','.join(map(str, batch))
+            item_list = item_list[self.page_limit:] 
+            url = f"https://xl-catalog-api.rozetka.com.ua/v4/goods/getDetails?product_ids={product_ids}"
+            print(url)
+            
+            resp = self.scraper.get(url)
+            ids = resp.json()
+            product_list.extend(
+                Product(id=int(item["id"]), brand=item["brand"], title=item["title"], price=float(item["price"]), href=item["href"])
+                for item in ids["data"]
+            )
+            print(len(product_list))
+        return product_list
 
 
-# data = get_total_pages_rozetka(f"https://xl-catalog-api.rozetka.com.ua/v4/goods/getDetails?country=UA&lang=ua&with_groups=1&with_docket=1&with_extra_info=1&goods_group_href=1&product_ids=354111201,477939314,358532337,234935635,12991699,413068041,387068100,505660054,83374176,429989186,135045840,234908281,277806733,316526614,358521642,93909002,323284093,323271967,53682576,393186270,425511654,131126259,399085038,506294609,506294189,506292129,364609773,234596293,180140570,358539690,417576903,506308029,275080888,383996817,266667191,442859777,395441364,453864368,441843560,506291429,396907284,503392249,316527697,131123333,436709108,504658874,364600170,503384109,484458999,504658919,450999611,437833892,408830295,422630754,381999168,422630745,484456084,9971798,390507354,41520760")
-# print(data)
+class MoyoScraper:
+    def __init__(self, url: str = "https://www.moyo.ua/ua/comp-and-periphery/inform_carrier/ssd/?page="):
+        self.url = url
+        try:
+            response = requests.get(url, headers=HEADERS)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            page_elements = soup.select("ul.pagination li[data-page]")
+            page_numbers = [int(li['data-page']) for li in page_elements if li.has_attr("data-page")]
+
+            if page_numbers:
+                self.count_pages = max(page_numbers)
+            else:
+                print("[WARN] No page numbers found")
+                self.count_pages = 1
+        except Exception as e: 
+            print(f"[ERROR] Failed to get total pages: {e}")
+            self.count_pages = 1
+
+
+    def fetch_html_moyo(self, timeout: int = 10) -> Optional[str]:
+        html_list = []
+        for page_num in range(self.count_pages):
+            try:
+                response = requests.get(f"{self.url}{page_num}", headers=HEADERS, timeout=timeout)
+                response.raise_for_status()
+                html_list.append(response.text)
+            except requests.RequestException as e:
+                print(f"[ERROR] Failed to fetch {self.url}: {e}")
+                return None
+        return html_list
+    
+
+Moyo = MoyoScraper()
+html = Moyo.fetch_html_moyo()
+parse_html = extract_products_from_html_moyo(html_list=html)
+save_to_csv_moyo(parse_html, f"products_moyo_{datetime.now().date()}.csv")
+
+
+
+kaka = RozetkaScraper()
+data = kaka.get_ids()
+baka = kaka.get_items(data)
+save_to_csv(baka, f"products_rozetka_{datetime.now().date()}.csv")
+
